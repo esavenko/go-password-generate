27b8e903193a0759281dfe152036e1e4cb822795
Egor Savenko <smahelzed@gmail.com>
2024-08-16 19:54:02 +0300

    Add .env variables

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -2,6 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
+  runtimeConfig: {
+    public: {
+      rootUrl: process.env.NUXT_PUBLIC_ROOT_URL ?? 'http://localhost:3000',
+      generateApi: process.env.NUXT_PUBLIC_API_GENERATE_API,
+      hashApi: process.env.NUXT_PUBLIC_API_HASH_API,
+    },
+  },
   modules: ['shadcn-nuxt', '@nuxtjs/tailwindcss', '@nuxt/image', '@pinia/nuxt'],
   shadcn: {
     prefix: '',
