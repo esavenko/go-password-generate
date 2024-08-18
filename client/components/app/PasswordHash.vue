3e843babfefcc859b449ff491ce48fa405a07465
Egor Savenko <smahelzed@gmail.com>
2024-08-18 21:29:08 +0300

    Add page for hashing personal passwords

--- client/components/app/PasswordHash.vue
+++ client/components/app/PasswordHash.vue
@@ -1,16 +1,36 @@
+<script lang="ts" setup>
+// stores
+import { useHashPassword } from '@/stores/hash.store';
+
+import { Input } from '@/components/ui/input';
+import { ClipboardCopy, Loader2 } from 'lucide-vue-next';
+
+const hashStore = useHashPassword();
+
+const enteredPassword = ref<string | null>('');
+
+const handleHash = async (): Promise<void> => {
+  if (enteredPassword.value) {
+    await hashStore.hashPassword(enteredPassword.value);
+    enteredPassword.value = hashStore.hashedPassword;
+  }
+};
+
+const disabledHashBtn = computed(() => enteredPassword.value!.length === 0);
+</script>
+
 <template>
   <section>
     <div class="flex items-center justify-center flex-col mt-10">
-      <span class="text-2xl">Тут вы можете сгенерировать себе пароль</span>
+      <span class="text-2xl">Тут вы можете хэшировать свой пароль</span>
 
       <div class="relative w-full mt-10">
         <Input
-          disabled
           id="search"
           type="text"
           placeholder="Сгенерировать пароль"
           class="h-20"
-          v-model="displayedPassword"
+          v-model="enteredPassword"
         />
         <span
           class="absolute end-0 inset-y-0 flex items-center justify-center px-2"
@@ -23,18 +43,13 @@
         <Button
           class="w-full h-12 text-base hover:bg-accent-foreground hover:text-slate-300 ease-in"
           variant="secondary"
-          @click="generatePassword"
+          @click="handleHash"
+          :disabled="disabledHashBtn"
         >
-          <Loader2 v-if="pending" class="w-4 h-4 mr-2 animate-spin" />
-          Сгенерировать пароль
-        </Button>
-
-        <Button
-          class="w-full h-12 text-base hover:bg-accent-foreground hover:text-slate-300 ease-in"
-          variant="secondary"
-          @click="hashPassword"
-        >
-          <Loader2 v-if="loading" class="w-4 h-4 mr-2 animate-spin" />
+          <Loader2
+            v-if="hashStore.isLoading"
+            class="w-4 h-4 mr-2 animate-spin"
+          />
           Хэшировать пароль
         </Button>
       </div>
